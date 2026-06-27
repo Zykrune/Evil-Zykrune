c4a2df773a5d2ee4d7488fa0dc25958fd7467de4
Zykrune <jmn122012@gmail.com>
2026-06-27 14:26:03 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "against Zykrune",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 5,
       },
     ],
   },
@@ -91,8 +91,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#0080ff",
+      secondary: "#0000ff",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
